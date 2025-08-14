17152d50a98f3e85766070ca30ac3cb8ac7af1e4
Maksim <166901118+amareigi@users.noreply.github.com>
2025-08-15 02:53:42 +0800

    delete build dir

--- build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/appqttest_res.cmake
+++
@@ -1,9 +0,0 @@
-
-set(target "appqttest")
-set(working_dir "C:/Users/amareigi/Documents/qttest")
-set(src_and_dest_list
-    "C:/Users/amareigi/Documents/qttest/src/view/calendar/MyCalendar.qml"
-    "C:/Users/amareigi/Documents/qttest/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/qttest/src/view/calendar/MyCalendar.qml"
-
-)
-set(timestamp_file "C:/Users/amareigi/Documents/qttest/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/appqttest_res.txt")
--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
     // Регистрируем контроллер в QML
     engine.rootContext()->setContextProperty("controller", controller);
 
-    const QUrl url(QStringLiteral("qrc:src//view/main_window.qml"));
+    const QUrl url(QStringLiteral("qrc:src/view/main_window.qml"));
     engine.load(url);
 
     return app.exec();
